6ed179ccef76223ebab6ff2cfa32beaa9f32efe4
kevendara <kevendara@gmail.com>
2021-07-25 12:03:04 -0500

    actualización y creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MiComponente } from './Components/mi-componente/mi-component.component';
+import { PeliculasComponent } from './Components/peliculas/peliculas.component';
+import { PruebasComponent } from './Components/pruebas/pruebas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MiComponente,
+    PeliculasComponent,
+    PruebasComponent
   ],
   imports: [
     BrowserModule
